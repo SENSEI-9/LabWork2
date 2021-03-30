48ead230b3d8ce8f3e5bae3bb91b503360a4d002
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-03-30 12:23:10 +0545

    Temperature check

--- 2question.no4.py
+++ 2question.no4.py
@@ -5,6 +5,7 @@ it's less than 10;
 it's a cold day; otherwise,
 it's neither hot nor cold.
 '''
+
 temperature=int(input('Enter the temperature :'))
 if temperature>30:
     print('it is a hot day')
