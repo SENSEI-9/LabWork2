5b5977e23642562155be2df8c1ca6ba0266ceda5
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-03-30 11:45:41 +0545

    Percentage check

---
+++ 2question.no4.py
@@ -0,0 +1,14 @@
+'''
+If temperature is greater than 30,
+it's a hot day otherwise if
+it's less than 10;
+it's a cold day; otherwise,
+it's neither hot nor cold.
+'''
+temperature=int(input('Enter the temperature :'))
+if temperature>30:
+    print('it is a hot day')
+elif temperature<10:
+    print('It is a cold day')
+else:
+    print('It is neither hot nor cold')
