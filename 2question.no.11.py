2ed200bfe2680cdf8774e25571be3c08267ca0d9
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-04-07 19:41:23 +0545

    finding smallest number amongst them

--- 2question.no.11.py
+++ 2question.no.11.py
@@ -1,10 +1,10 @@
 '''given three integers, print the smallest one. (three integers should be user input)'''
-a=int(input('enter first number'))
-b=int(input('enter second number'))
-c=int(input('enter third number'))
+a=int(input('Enter first number : '))
+b=int(input('Enter second number : '))
+c=int(input('Enter third number : '))
 if a<b and a<c:
-    print('a is the smallest number among them')
+    print(f'{a} is the smallest number among {a},{b},{c}')
 elif b<a and b<c:
-    print(' b is the smallest number among them')
+    print(f'{b} is the smallest number among {a},{b},{c}')
 else:
-    print('c is the smallest number among them')
+    print(f'{c} is the smallest number among {a},{b},{c}')
